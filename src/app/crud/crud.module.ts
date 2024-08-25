2a2ad13b6a42d96eefdafb44109377762b9f198b
Giovanicoelho328 <coelhogiovani1@gmail.com>
2024-08-25 11:53:54 +0100

    criar um componente

---
+++ src/app/crud/crud.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { ProductListComponent } from './product-list/product-list.component';
+import { ProductFormComponent } from './product-form/product-form.component';
+import { ProductDetailsComponent } from './product-details/product-details.component';
+
+
+
+@NgModule({
+  declarations: [
+    ProductListComponent,
+    ProductFormComponent,
+    ProductDetailsComponent
+  ],
+  imports: [
+    CommonModule
+  ]
+})
+export class CRUDModule { }
