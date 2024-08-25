c83b7eb8afe4adb5b78d49120191a84b55b217f6
Giovanicoelho328 <coelhogiovani1@gmail.com>
2024-08-25 13:03:18 +0100

    configuracao de roteamento adicionado
    desde o modulo crud-2 a app principal consegue comunicar com os componentes que estao dentro do modulo crud-2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'crud-2', pathMatch: 'full'},
+  {path: 'crud-2', loadChildren: ()=>import('./crud-2/crud-2.module').then(m=>m.Crud2Module)}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/crud-2/crud-2.module.ts
+++ src/app/crud-2/crud-2.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
+
+import { Crud2RoutingModule } from './crud-2-routing.module';
 import { ProductListComponent } from './product-list/product-list.component';
 import { ProductFormComponent } from './product-form/product-form.component';
 import { ProductDetailsComponent } from './product-details/product-details.component';
-
 
 
 @NgModule({
@@ -13,7 +14,8 @@ import { ProductDetailsComponent } from './product-details/product-details.compo
     ProductDetailsComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    Crud2RoutingModule
   ]
 })
-export class CRUDModule { }
+export class Crud2Module { }
